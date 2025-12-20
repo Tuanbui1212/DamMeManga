3bf0c41b07009b892c3eb66c6a4472734da2660f
Bui Anh Tuan <tuanbui.code@gmail.com>
2025-12-20 23:50:07 +0700

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import React, { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./presentation/Layouts/DefaultLayout";
+import { publicRoutes, privateRoutes } from "./presentation/routes";
+import AdminRoute from "./presentation/routes/AdminRoute";
+import "./lib/chart";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {/* ROUTE CÔNG KHAI */}
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = route.layout ?? DefaultLayout;
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+
+          {/* ROUTE RIÊNG TƯ (bọc AdminRoute) */}
+          {privateRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = route.layout ?? DefaultLayout;
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <AdminRoute>
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  </AdminRoute>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
