52d7ae368d5484210468809d8f03b1b74fd7966f
Bui Anh Tuan <tuanbui.code@gmail.com>
2025-12-21 02:34:01 +0700

    update

--- frontend/src/infrastructure/http/axiosClient.js
+++ frontend/src/infrastructure/http/axiosClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL || "http://localhost:8080") + "/api",
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
